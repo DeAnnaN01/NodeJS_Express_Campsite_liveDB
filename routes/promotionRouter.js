b77bb960f804a3a7c5b1a6780ec32a41077300c0
DeAnna Nicholson <13dpn13@gmail.com>
2023-08-22 21:48:55 -0700

    workshop 2 complete--before jump ahead to passportJWT

--- routes/promotionRouter.js
+++ routes/promotionRouter.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Promotion = require('../models/promotion.js');
 const promotionRouter = express.Router();
 
 
@@ -12,23 +13,22 @@ promotionRouter.route('/')
     .get(async (req, res) => {
         const promotions = await Promotion.find();
         res.send(promotions);
-        res.end('Will send all the promotions to you');
+        res.end();
     })
     .post(async (req, res, next) => {
         await Promotion.create(req.body)
-            .then(promotions=> {
-                console.log('Promotion Created ', promotions);
-                res.setHeader('Content-Type', 'application/json');
-                res.json(promotions);
-            })
-            .catch(err => next(err));
-        res.end(`Will add the promotion: ${req.body.name} with description: ${req.body.description}`);
+        .then(promotions => {
+            console.log('Promotion Created ', promotions);
+            res.json(promotions);
+        })
+        .catch(err => next(err));
     })
     .put((req, res) => {
         res.statusCode = 403;
         res.end('PUT operation not supported on /promotions');
     })
-    .delete((req, res) => {
+    .delete(async (req, res) => {
+        await Promotion.deleteMany();
         res.end('Deleting all promotions');
     });
 
@@ -40,20 +40,32 @@ promotionRouter.route('/:promotionId')
         res.setHeader('Content-Type', 'text/plain');
         next();
     })
-    .get((req, res) => {
+    .get(async (req, res) => {
+        await Promotion.findById(req.params.promotionId);
         res.end(`Will send details of the promotion: ${req.params.promotionId} to you`);
     })
     .post((req, res) => {
         res.statusCode = 403;
         res.end(`POST operation not supported on /promotions/${req.params.promotionId}`);
     })
-    .put((req, res) => {
+    .put(async (req, res) => {
+        const filter = { id: req.params.promotionId };
+        const update = {
+            name: req.body.name,
+            image: req.body.image,
+            featured: req.body.featured,
+            cost: req.body.cost,
+            description: req.body.description
+        };
+        await Promotion.findOneAndUpdate(filter, update, {new: true});
+        console.log(filter, update);
         res.write(`Updating the promotion ${req.params.promotionId}\n`);
-        res.end(`Will update the promotion: ${req.body.name} with description: ${req.body.description}`);
+        res.end(`Will update the promotion: ${req.body.name} \n\twith the cost: ${req.body.cost} \n\twith description: ${req.body.description}`);
     })
-    .delete((req, res) => {
+    .delete(async (req, res) => {
+        await Promotion.findByIdAndDelete(req.params.promotionId);
         res.end(`Deleting promotion ${req.params.promotionId}`);
-    })
+    });
 
 
 module.exports = promotionRouter;
